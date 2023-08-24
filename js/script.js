7c192a60b09b4f6cdccf5585d153f036e4e3a61d
Alexei <103207831+Dubrovschii@users.noreply.github.com>
2023-08-24 12:17:18 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ function postData(form) {
   
       const request = new XMLHttpRequest();
       request.open('POST', 'server.php');
-      // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
       const formData = new FormData(form);
 
       const object = {};
